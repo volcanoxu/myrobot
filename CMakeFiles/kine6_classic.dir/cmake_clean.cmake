06bdba5c210d471d72b7f79aa4f263d4be6a2816
volcano <xucan2018@gmail.com>
2019-08-12 12:02:56 +0800

    myrobot race99 #1 commit

---
+++ CMakeFiles/kine6_classic.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/kine6_classic.dir/main.c.o"
+  "CMakeFiles/kine6_classic.dir/Classic6dofKine.c.o"
+  "kine6_classic.pdb"
+  "kine6_classic.exe"
+  "libkine6_classic.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/kine6_classic.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
